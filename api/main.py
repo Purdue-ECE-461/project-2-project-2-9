aa53b792aabc7abf31f38f47e7d00b405e68de70
jbhelf <jhelfman@purdue.edu>
2021-12-02 11:56:24 -0500

    Added base testing

--- api/main.py
+++ api/main.py
@@ -24,47 +24,6 @@ app = Flask(__name__)
 auth = firebase.auth()
 api=Api(app)
 
-#Get Del Put to make for this specific webpage (I want to make sure this works before writing the framework for everything else)
-# @app.route("/packages/offset=<offset>", methods=['GET','POST'])
-# def post_package_offset(): #URL: http://127.0.0.1:8001/packages
-#     #Check if there is an offset given (http://127.0.0.1:8001/packages/offset=?)
-#     # offset = flask.request.args.get('offset')
-
-#     #Just a placeholder for the data that would be returned
-#     info = {
-#         "Name": "string",
-#         "Version": "1.2.3",
-#         "ID": "string"
-#     }
-
-#     #This is the official response that will be recieved (error code and data)
-#     response = app.response_class(
-#         response=json.dumps(info),
-#         status=201,
-#         mimetype='application/json'
-#     )
-#     return response
-
-# @app.route("/packages/", methods=['GET','POST'])
-# def post_package(): #URL: http://127.0.0.1:8001/packages
-#     #Check if there is an offset given (http://127.0.0.1:8001/packages/)
-#     # offset = 1
-#     #Just a placeholder for the data that would be returned
-#     info = {
-#         "Name": "string",
-#         "Version": "1.2.3",
-#         "ID": "string"
-#     }
-
-#     #This is the official response that will be recieved (error code and data)
-#     response = app.response_class(
-#         response=json.dumps(info),
-#         status=201,
-#         mimetype='application/json'
-#     )
-    
-#     return response
-
 """
 Begin helper functions:
 """
--- api/test_api.py
+++ api/test_api.py
@@ -1,12 +1,120 @@
 import unittest
 import main as tasks
 import json
+import ast
 
 class TestSuite(unittest.TestCase): 
     #We have hardcoded the API responses FOR NOW and we will be testing to
     # ensure that the data responses and types are as expected.
-    pass
-    #NOTE: We are only testing the 'post_package' method.  We do not want to use the offset method as it epects an offset, but functions the same as 'post_package'.
+    
+    def test_packageRetrieve(self):
+        #Testing invalid credentials:
+        bad_case = tasks.packageRetrieve(0)
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('Error!  You do not have the permissions to view this item!', response_message)
+
+        return
+
+    def test_updatePackageVersion(self):
+        #Testing invalid credentials:
+        bad_case = tasks.updatePackageVersion(0)
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('Error!  You do not have the permissions to view this item!', response_message)
+
+        return
+    
+    def test_deletePackageVersion(self):
+        #Testing invalid credentials:
+        bad_case = tasks.deletePackageVersion(0)
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('Error!  You do not have the permissions to view this item!', response_message)
+
+        return
+
+    def test_ratePackage(self):
+        #Testing invalid credentials:
+        bad_case = tasks.ratePackage(0)
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('Error!  You do not have the permissions to view this item!', response_message)
+
+        return
+
+    def test_resetRegistry(self):
+        #Testing invalid credentials:
+        bad_case = tasks.resetRegistry()
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('You do not have permission to reset the registry.', response_message)
+
+        return
+
+    def test_getPackages(self):
+        #Testing invalid credentials:
+        bad_case = tasks.getPackages()
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('You do not have permission to view the registry.', response_message)
+
+        return
+
+    def test_createPackage(self):
+        #Testing invalid credentials:
+        bad_case = tasks.createPackage()
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('You do not have permission to add to the registry.', response_message)
+
+        return
+    
+    def test_getPackageByName(self):
+        #Testing invalid credentials:
+        bad_case = tasks.getPackageByName("test")
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('You do not have permission to view the package.', response_message)
+
+        return
+    
+    def test_deletePackageVersions(self):
+        #Testing invalid credentials:
+        bad_case = tasks.deletePackageVersions("test")
+        status_code = bad_case._status_code
+        self.assertEqual(401, status_code)
+        response_code = ast.literal_eval(bad_case.response[0].decode("utf-8"))['code']
+        self.assertEqual(401, response_code)
+        response_message = ast.literal_eval(bad_case.response[0].decode("utf-8"))['message']
+        self.assertEqual('You do not have permission to modify the package.', response_message)
+
+        return
+    # #NOTE: We are only testing the 'post_package' method.  We do not want to use the offset method as it epects an offset, but functions the same as 'post_package'.
       
     # def test_basicAPI_response(self):
     #     #Testing the API response.
@@ -15,9 +123,9 @@ class TestSuite(unittest.TestCase):
     #     status_response = tasks.post_package().status
     #     self.assertEqual("201 CREATED", status_response)
 
-    #     # with self.subTest(key=201):
-    #     #     status_response = tasks.post_package().status
-    #     #     self.assertEqual(201, status_response)
+        # with self.subTest(key=201):
+        #     status_response = tasks.post_package().status
+        #     self.assertEqual(201, status_response)
         
     # def test_basicAPI_mime(self):
     #     #Testing the API mimetype attribute.
@@ -57,8 +165,17 @@ class TestSuite(unittest.TestCase):
 # or
 # pytest test_api.py
 
+from flask import Flask, jsonify
+app = Flask(__name__)
+def convertJSONFormat(code, data):
+    response = app.response_class(
+        response=json.dumps(data),
+        status=code,
+        mimetype='application/json'
+    )
+    return response
+
 if __name__ == '__main__':
-
-
+    
     unittest.main()
     exit(0)
