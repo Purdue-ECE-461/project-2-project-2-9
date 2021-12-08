665cb642c02c816e4547234931a6171faa58250f
rudranshagrawal <66431586+rudranshagrawal@users.noreply.github.com>
2021-12-08 16:13:51 -0500

    Added Dependency Score

--- rate/main.py
+++ rate/main.py
@@ -71,11 +71,11 @@ def main() -> None:
         repoList.append((repoUrl, repoMetrics.netScore))
         repoMetricsDict[repoUrl] = repoMetrics
 
-    print('URL NET_SCORE RAMP_UP_SCORE CORRECTNESS_SCORE BUS_FACTOR_SCORE RESPONSIVE_MAINTAINER_SCORE LICENSE_SCORE')
+    print('URL NET_SCORE RAMP_UP_SCORE CORRECTNESS_SCORE BUS_FACTOR_SCORE RESPONSIVE_MAINTAINER_SCORE LICENSE_SCORE DEPENDENCY_SCORE')
     for repoUrl, _ in reversed(sorted(repoList, key=lambda x: x[1])):
         repoMetrics = repoMetricsDict[repoUrl]
-        # print(f"{repoUrl} {repoMetrics.netScore} {repoMetrics.rampUpScore} {repoMetrics.correctnessScore} {repoMetrics.busFactorScore} {repoMetrics.responsiveMaintainerScore} {repoMetrics.licenseScore}")
-        return repoMetrics.netScore, repoMetrics.rampUpScore, repoMetrics.correctnessScore, repoMetrics.busFactorScore, repoMetrics.responsiveMaintainerScore, repoMetrics.licenseScore
+        print(f"{repoUrl} {repoMetrics.netScore} {repoMetrics.rampUpScore} {repoMetrics.correctnessScore} {repoMetrics.busFactorScore} {repoMetrics.responsiveMaintainerScore} {repoMetrics.licenseScore} {repoMetrics.dependencyScore}")
+#         return repoMetrics.netScore, repoMetrics.rampUpScore, repoMetrics.correctnessScore, repoMetrics.busFactorScore, repoMetrics.responsiveMaintainerScore, repoMetrics.licenseScore
 
 
 def call_main(url):
